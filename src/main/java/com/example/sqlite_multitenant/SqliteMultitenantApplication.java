bd1956c83161bc39c9dd11e126622c8c9ff57e97
Paweł Polakiewicz <pp.polakiewicz@gmail.com>
2024-01-15 23:50:22 +0100

    Initial commit

---
+++ src/main/java/com/example/sqlite_multitenant/SqliteMultitenantApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlite_multitenant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqliteMultitenantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqliteMultitenantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sqlite_multitenant/SqliteMultitenantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlite_multitenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqliteMultitenantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
